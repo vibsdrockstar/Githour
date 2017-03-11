ac854edbc34e73d5630dd37e0f42b83f8e856be5
Vibhor <bijoy.vibhor19@gmail.com>
2017-03-11 19:40:17 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+package com.example.vibhor.githour;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void openIntroduction(View view) {
+        startActivity(new Intent(this, Introduction.class));
+    }
+}
